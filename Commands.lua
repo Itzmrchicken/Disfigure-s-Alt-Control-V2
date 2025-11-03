08bfa125c6fd781dad16bd5c46cde3406752b5e1
ltzmrchicken <130897982+Itzmrchicken@users.noreply.github.com>
2025-11-03 17:26:56 -0500

    Add command definitions and improve structure

--- Commands.lua
+++ Commands.lua
@@ -6,6 +6,20 @@ local Commands = {
 		
 		Args = {"player"},
 		
+		Definition = "Teleports the bots to the provided player",
+		
+		Run = function(Runner: Player, ...)
+			print(Runner, HttpService:JSONEncode(...))
+		end,
+	},
+	
+	define = {
+		Aliases = {"def"},
+		
+		Args = {"command"},
+		
+		Definition = "Defines a provided command",
+		
 		Run = function(Runner: Player, ...)
 			print(Runner, HttpService:JSONEncode(...))
 		end,
