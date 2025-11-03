5b8e1a0e39aa552a5f21b2cf3318c27ee4c02aac
ltzmrchicken <130897982+Itzmrchicken@users.noreply.github.com>
2025-11-03 17:21:39 -0500

    Implement bot command handling and messaging

--- Main.lua
+++ Main.lua
@@ -1,1 +1,186 @@
+local ReplicatedStorageService = game:GetService("ReplicatedStorage")
+local TextChatService = game:GetService("TextChatService")
+local HttpService = game:GetService("HttpService")
+local PlayersService = game:GetService("Players")
 
+local LocalPlayer = PlayersService.LocalPlayer
+
+local Data = getgenv().Data
+
+local CommandsModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/Itzmrchicken/Disfigure-s-Alt-Control-V2/refs/heads/main/Commands.lua"))()
+
+local Master = Data["Master"]
+local Prefix = Data["Prefix"]
+local Bots = Data["Bots"]
+
+local AccountIsMaster = LocalPlayer.Name == Master or LocalPlayer.DisplayName == Master
+local IsDisplayName = LocalPlayer.DisplayName == Master
+
+local BotIndex = not AccountIsMaster and table.find(Bots, LocalPlayer.Name)
+
+local CustomArgs = {
+	me = function(Runner: Player)
+		return Runner
+	end,
+}
+
+local ArgumentTypes = {
+	player = function(Runner: Player, ArgString: string, Index: number, ArgText: string)
+		for _, player in PlayersService:GetPlayers() do
+			if ArgText and (player.Name:lower():sub(1, #ArgText) == ArgText or player.DisplayName:lower():sub(1, #ArgText) == ArgText) then
+				return player
+			end
+		end
+		
+		if CustomArgs[ArgText] then
+			return CustomArgs[ArgText](Runner)
+		end
+		
+		return "Player not found or nil", true
+	end,
+}
+
+function debug_style(Type: string, FunctionName: string, ...)
+	return "["..Type.."]["..FunctionName.."] ".. ...
+end
+
+function verify_all_bots()
+	for _, bot in Bots do
+		if not PlayersService:FindFirstChild(bot) then
+			table.remove(Bots, table.find(Bots, bot))
+			
+			print(bot, "doesn't exist in-game")
+		end
+	end
+	
+	BotIndex = not AccountIsMaster and table.find(Bots, LocalPlayer.Name)
+end
+
+function bot_response(Bot: number, Message: string)
+	local TCS_Version = TextChatService.ChatVersion
+	
+	if Bot then
+		if Bot == BotIndex then
+			if TCS_Version == Enum.ChatVersion.TextChatService then
+				local General_Channel: TextChannel = TextChatService.TextChannels.RBXGeneral
+
+				General_Channel:SendAsync(Message)
+			else
+				ReplicatedStorageService:WaitForChild("DefaultChatSystemChatEvents").SayMessageRequest:FireServer(Message, "All")
+			end
+
+			return
+		end
+	else
+		if TCS_Version == Enum.ChatVersion.TextChatService then
+			local General_Channel: TextChannel = TextChatService.TextChannels.RBXGeneral
+
+			General_Channel:SendAsync(Message)
+		else
+			ReplicatedStorageService:WaitForChild("DefaultChatSystemChatEvents").SayMessageRequest:FireServer(Message, "All")
+		end
+	end
+end
+
+function account_master()
+	print(debug_style("INFO", "account_master()", "Account is master"))
+end
+
+function grab_args(Runner: Player, Command: string, Arguments)
+	if not(Arguments or next(Arguments)) then return warn(debug_style("WARN", "grab_args()", "Can't run command in general. No args")) end
+	
+	local CommandData = {}
+	local CmdArgs = {}
+	
+	Command = Command:split(".")[2]
+	
+	if CommandsModule[Command] then
+		CommandData = CommandsModule[Command]
+		
+		for Index, Arg in CommandsModule[Command].Args do
+			local ArgumentData, IsError = ArgumentTypes[Arg](Runner, Arg, Index, Arguments[Index])
+			
+			if ArgumentData and not IsError then
+				CmdArgs[Arg] = ArgumentData
+			else
+				warn(debug_style("WARN", "grab_args()", IsError and ArgumentData))
+				
+				return
+			end
+		end
+	else
+		for cmd, cmd_data in CommandsModule do
+			local Aliases = cmd_data.Aliases
+						
+			if table.find(Aliases, Command) then
+				print("Found command")
+				
+				for Index, Arg in cmd_data.Args do
+					local ArgumentData, IsError = ArgumentTypes[Arg](Runner, Arg, Index, Arguments[Index])
+
+					if ArgumentData and not IsError then
+						CmdArgs[Arg] = ArgumentData
+					else
+						warn(debug_style("WARN", "grab_args()", IsError and ArgumentData))
+
+						return
+					end
+				end
+				
+				CommandData = cmd_data
+				
+				break
+			end
+		end
+		
+		if not next(CmdArgs) then
+			warn("Invalid command", Command)
+		end
+	end
+	
+	return CommandData, CmdArgs
+end
+
+function run_command(Runner: Player, CommandData, Arguments)
+	if CommandData and CommandData.Run then
+		CommandData.Run(Runner, Arguments)
+		
+		return true
+	end
+	
+	return
+end
+
+function register_command(Runner: Player, Text: string)
+	local Split = Text:split(" ")
+	
+	local Command = Split[1]:find(Prefix, 1) and Split[1]
+	
+	local Arguments = table.remove(Split, 1) and Split
+	
+	if Command and next(Arguments) then
+		local CommandData, CommandArgs = grab_args(Runner, Command, Arguments)
+		
+		if CommandArgs and next(CommandArgs) then
+			local Status = run_command(Runner, CommandData, CommandArgs)
+			
+			print(Status)
+		end
+	end
+end
+
+return function()
+	if AccountIsMaster then return account_master() end
+	
+	verify_all_bots()
+		
+	TextChatService.MessageReceived:Once(function(message)
+		local PlayerMessage: Player = PlayersService:FindFirstChild(message.TextSource.Name)
+		
+		if IsDisplayName and PlayerMessage.DisplayName == Master or PlayerMessage.Name == Master then
+			print(debug_style("INFO", "main() => MessageReceived", message.Text))
+			
+			register_command(PlayerMessage, message.Text)
+		end
+	end)
+end
