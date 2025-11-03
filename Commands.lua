03a22d510af39fd91c1c8895e55e8e408af50877
ltzmrchicken <130897982+Itzmrchicken@users.noreply.github.com>
2025-11-03 18:49:33 -0500

    Enhance teleport command with botindex argument
    Updated teleport command to include botindex for precise positioning.

--- Commands.lua
+++ Commands.lua
@@ -1,17 +1,40 @@
 local HttpService = game:GetService("HttpService")
+local PlayersService = game:GetService("Players")
+
+local LocalPlayer = PlayersService.LocalPlayer
 
 local FunctionsModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/Itzmrchicken/Disfigure-s-Alt-Control-V2/refs/heads/main/functions.lua"))()
+
+local Data = getgenv().Data
+
+local Master = Data["Master"]
+local Bots = Data["Bots"]
 
 local Commands = {
 	teleport = {
 		Aliases = {"goto", "to", "tp"},
 		
-		Args = {"player"},
+		Args = {"player", "botindex"},
 		
 		Definition = "Teleports the bots to the provided player",
 		
 		Run = function(Runner: Player, Data)
-			print(Runner, HttpService:JSONEncode(Data))
+			local BotIndex: number = Data.botindex
+			local Target: Player = Data.Player
+			
+			local LPCharacter = LocalPlayer.Character
+			
+			local Character = Target.Character
+			
+			if LPCharacter and Character then
+				local LPHumanoidRootPart: BasePart = LPCharacter:FindFirstChild("HumanoidRootPart")
+				
+				local HumanoidRootPart: BasePart = Character:FindFirstChild("HumanoidRootPart")
+				
+				if LPHumanoidRootPart and HumanoidRootPart then
+					LPHumanoidRootPart.CFrame = HumanoidRootPart.CFrame * CFrame.new(0, 0, (1 * BotIndex))
+				end
+			end
 		end,
 	},
 	
