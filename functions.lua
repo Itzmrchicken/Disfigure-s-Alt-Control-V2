a1253b2636bc51b607e7efff16568189636f37f7
ltzmrchicken <130897982+Itzmrchicken@users.noreply.github.com>
2025-11-03 19:59:23 -0500

    Remove CommandsModule and command data handling
    Removed the CommandsModule loading and related command data functionality.

--- functions.lua
+++ functions.lua
@@ -3,8 +3,6 @@ local TextChatService = game:GetService("TextChatService")
 
 local Functions = {}
 
-local CommandsModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/Itzmrchicken/Disfigure-s-Alt-Control-V2/refs/heads/main/Commands.lua"))()
-
 function Functions.Chat(Bot: number, BotIndex: number, Message: string)
 	local TCS_Version = TextChatService.ChatVersion
 
@@ -29,22 +27,6 @@ function Functions.Chat(Bot: number, BotIndex: number, Message: string)
 			ReplicatedStorageService:WaitForChild("DefaultChatSystemChatEvents").SayMessageRequest:FireServer(Message, "All")
 		end
 	end
-end
-
-function Functions.CommandData(Command: string)
-	if CommandsModule[Command] then
-		return CommandsModule[Command]
-	else
-		for cmd, cmd_data in CommandsModule do
-			local Aliases = cmd_data.Aliases
-			
-			if table.find(Aliases, Command) then
-				return cmd_data
-			end
-		end
-	end
-	
-	return "Couldn't find command "..Command, true
 end
 
 return Functions
