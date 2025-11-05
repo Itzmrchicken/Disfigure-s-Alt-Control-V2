46d772ccce03c8d1e1ba82e8b932c8ccfc9ae692
ltzmrchicken <130897982+Itzmrchicken@users.noreply.github.com>
2025-11-05 00:44:45 -0500

    Update Commands.lua

--- Commands.lua
+++ Commands.lua
@@ -443,7 +443,7 @@ local Commands = {
 				
 				LPHumanoidRootPart.CFrame = HumanoidRootPart.CFrame * CFrame.new(0, 0, math.random(-10, 10)) + Humanoid.MoveDirection
 				
-				if (Humanoid.Health <= 0 or LPHumanoid.Health <= 0) or LocalPlayer:DistanceFromCharacter(HumanoidRootPart.Position) > 25 then
+				if (Humanoid.Health <= 0 or LPHumanoid.Health <= 0) or not(Character or LPCharacter) or LocalPlayer:DistanceFromCharacter(HumanoidRootPart.Position) > 25 then
 					Connections.RunService.Fling:Disconnect()
 					
 					Connections.RunService.Fling = nil
